ee58d482b6334805d778648b7fe2b39b1fd2e05a
astisk <pobierac@gmail.com>
2024-04-04 18:38:24 +0200

    Prototype of inventory

---
+++ Scripts/Item.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoarder.Scripts
+{
+	public partial class Item : Node2D
+	{
+		public override void _Ready()
+		{
+			Resource resource;
+			TextureRect child = GetNode<TextureRect>("TextureRect");
+
+			if (new Random().Next(0, 2) == 0)
+				resource = ResourceLoader.Load("res://Graphics/Equippable/weapon.png");
+			else
+				resource = ResourceLoader.Load("res://Graphics/Equippable/pickaxe.png");
+
+			Texture2D texture = (Texture2D)resource;
+			child.Texture = texture;
+		}
+	}
+}
---
+++ Scripts/Slot.cs
@@ -0,0 +1,22 @@
+﻿using Godot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hoarder.Scripts
+{
+    public partial class Slot : Panel
+    {
+        PackedScene ItemClass = (PackedScene)ResourceLoader.Load("res://Scenes//Item.tscn");
+        Node item;
+        public override void _Ready()
+        {
+             item = ItemClass.Instantiate();
+            AddChild(item);
+            Node2D child = GetNode<Node2D>("Item");
+            child.Position = new Vector2(5, 5);
+        }
+    }
+}
