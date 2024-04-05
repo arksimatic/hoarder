89e40c0ed493f356483135addcf8e5edd505d86a
astisk <pobierac@gmail.com>
2024-04-05 12:12:17 +0200

    Initial code for inventory item moving

--- Scripts/Slot.cs
+++ Scripts/Slot.cs
@@ -9,14 +9,31 @@ namespace Hoarder.Scripts
 {
     public partial class Slot : Panel
     {
-        PackedScene ItemClass = (PackedScene)ResourceLoader.Load("res://Scenes//Item.tscn");
-        Node item;
+        private PackedScene ItemClass = (PackedScene)ResourceLoader.Load("res://Scenes//Item.tscn");
+        private Node2D _item;
+        private GridContainer _inventoryGrid;
         public override void _Ready()
         {
-             item = ItemClass.Instantiate();
-            AddChild(item);
+            _inventoryGrid = GetNode<GridContainer>("Inventory");
+            _item = ItemClass.Instantiate() as Node2D;
+            AddChild(_item);
             Node2D child = GetNode<Node2D>("Item");
             child.Position = new Vector2(5, 5);
+        }
+
+        private void PickFromSlot()
+        {
+            RemoveChild(_item);
+            _inventoryGrid.AddChild(_item);
+            _item = null;
+        }
+
+        private void PutIntoSlot(Node2D newItem)
+        {
+            _item = newItem;
+            _item.Position = new Vector2(0, 0);
+            _inventoryGrid.RemoveChild(_item);
+            AddChild(_item);
         }
     }
 }
