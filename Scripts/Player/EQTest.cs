da3436594f502423c317263ded49bf180e7d462e
astisk <pobierac@gmail.com>
2024-04-24 21:03:58 +0200

    .

--- Scripts/Player/EQTest.cs
+++ Scripts/Player/EQTest.cs
@@ -4,16 +4,16 @@ namespace Hoarder.Scripts
 {
 	public partial class EQTest : Node2D
 	{
-        private CharacterBody2D _parent;
+        	private CharacterBody2D _parent;
 
 		public override void _Ready()
 		{
-            _parent = (CharacterBody2D)GetParent();
-        }
+            		_parent = (CharacterBody2D)GetParent();
+        	}
 
 		public override void _Process(double delta)
 		{
-            GlobalPosition = HMath.TrimToCircle(GetGlobalMousePosition(), _parent.GlobalPosition, 20);
+            		GlobalPosition = HMath.TrimToCircle(GetGlobalMousePosition(), _parent.GlobalPosition, 20);
 		}
-    }
+    	}
 }
