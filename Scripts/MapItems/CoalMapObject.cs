d4ee87ee0e87417b977e78e5e9a0e7aab7c8dede
astisk <pobierac@gmail.com>
2024-05-02 13:34:57 +0200

    Changes toward items on ground

--- Scripts/MapItems/CoalMapObject.cs
+++ Scripts/MapItems/CoalMapObject.cs
@@ -8,16 +8,5 @@ public partial class CoalMapObject : MapObject
 		//_customSignals = GetNode<CustomSignals>("/root/Scripts/CustomSignals");
 		//_customSignals.DamageObject += OnDamaged;
 	}
-	public override void OnDamaged(Int32 damage)
-	{
-		Health -= damage;
-		if (Health <= 0)
-		{
-			DestroyCoal();
-		}
-	}
-	public void DestroyCoal()
-	{
-		QueueFree();
-	}
+
 }
--- Scripts/MapItems/MapObject.cs
+++ Scripts/MapItems/MapObject.cs
@@ -4,5 +4,12 @@ using System;
 public partial class MapObject : Node2D
 {
 	public virtual Int32 Health { get; set; }
-	public virtual void OnDamaged(Int32 damage) { }
+    public virtual void OnDamaged(Int32 damage)
+    {
+        Health -= damage;
+        if (Health <= 0)
+        {
+            QueueFree();
+        }
+    }
 }
